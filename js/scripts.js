ce57f37f051f591b262578f07952202d0b2810cb
florian-grass <89141302+florian-grass@users.noreply.github.com>
2021-08-31 21:32:37 +0200

    Update scripts.js
    Tried in two different ways to insert the IIFE part. Both did not work. They are commented out.
    
    Now only working the Part 1 from the task

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,74 @@
 /* Pokedex */
-// Task 1.2 - Object Array
-let pokemonList = [
-  { name: 'Gloom',
-    height: 2.07,
-    types: ['Grass', 'Poison']
-  },
+// Task 1.5 - Object Array
 
-  { name: 'Parasect',
-    height: 3.03,
-    types: ['Bug', 'Grass']
-  },
+//let pokemonRepository = function () {
+  let pokemonList = [
+    { name: 'Gloom',
+      height: 2.07,
+      types: ['Grass', 'Poison']
+    },
 
-  { name: 'Persian',
-    height: 3.03,
-    types: ['Normal']
-  },
+    { name: 'Parasect',
+      height: 3.03,
+      types: ['Bug', 'Grass']
+    },
 
-  { name: 'Primeape',
-    height: 3.03,
-    types: ['Fighting']
-  },
+    { name: 'Persian',
+      height: 3.03,
+      types: ['Normal']
+    },
 
-  { name: 'Poliwrath',
-    height: 4.03,
-    types: ['Water', 'Fighting']
-  },
-  { name: 'Bulbasaur',
-    height: 0.7,
-    types: ['Grass', 'Poison']
+    { name: 'Primeape',
+      height: 3.03,
+      types: ['Fighting']
+    },
+
+    { name: 'Poliwrath',
+      height: 4.03,
+      types: ['Water', 'Fighting']
+    },
+    { name: 'Bulbasaur',
+      height: 0.7,
+      types: ['Grass', 'Poison']
+    }
+  ];
+
+// returning pokemonList first try
+/*
+  return {
+    pokemonRepository.getAll(){
+      return pokemonList;
+    }
+    pokemonRepository.add(item) {
+      return pokemonList;
+    },
   }
-];
+*/
+// returning pokemonList second try
+/*
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll
+  };
+*/
 
 
-pokemonList.forEach(function(pokemon) {
+  pokemonList.forEach(function(pokemon) {
 
 
-  if (pokemon.height > 4) {
-      document.write(pokemon.name + " (height: " + pokemon.height + " inch)" + " - Wow, that's big! , " + "<br >");
-  } else {document.write(pokemon.name + " (height: " + pokemon.height + " inch)" + ", " + "<br >");
-      }
-})
-
+    if (pokemon.height > 4) {
+        document.write(pokemon.name + " (height: " + pokemon.height + " inch)" + " - Wow, that's big! , " + "<br >");
+    } else {document.write(pokemon.name + " (height: " + pokemon.height + " inch)" + ", " + "<br >");
+        }
+  })
+  
+//})();
 /*
 for (let i = 0; i < pokemonList.length; i++) {
 
